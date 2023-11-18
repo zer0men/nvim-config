ed718fd68cad2880ca84fe1d84f76b99e0b530c6
Marko Roman <marko1299@ukr.net>
2023-11-18 22:32:40 +0200

    Update trissitter

--- lua/zer0/plugins/treesitter/init.lua
+++ lua/zer0/plugins/treesitter/init.lua
@@ -2,7 +2,7 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	config = function()
 		require("nvim-treesitter.configs").setup({
-			ensure_installed = { "all" },
+			ensure_installed = "all",
 			sync_install = true,
 		})
 		vim.cmd.TSUpdate()
