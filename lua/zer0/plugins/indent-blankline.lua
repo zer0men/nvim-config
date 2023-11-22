0c6d568476408d182fd8a4eb1a4aa592ca2b8da9
Marko Roman <marko1299@ukr.net>
2023-11-22 08:28:28 +0200

    Add anothe maybe usefull plugins

---
+++ lua/zer0/plugins/indent-blankline.lua
@@ -0,0 +1,12 @@
+return {
+	"lukas-reineke/indent-blankline.nvim",
+	main = "ibl",
+	opts = {},
+	init = function()
+		vim.opt.list = true
+		vim.opt.listchars:append("eol:↴")
+		vim.opt.listchars:append("space:.")
+		vim.opt.listchars:append("trail:-")
+		vim.opt.listchars:append("nbsp:_")
+	end,
+}
