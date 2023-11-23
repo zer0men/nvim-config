7126cd17556e557f0351d0dc9f5697bf1c94321d
Marko Roman <marko1299@ukr.net>
2023-11-23 17:58:36 +0200

    add lualine and lsp-progress

---
+++ lua/zer0/plugins/lsp/lsp-progress.lua
@@ -0,0 +1,10 @@
+return {
+	"linrongbin16/lsp-progress.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	init = function()
+		vim.g.lualine_c = require("lsp-progress").progress
+	end,
+	config = function()
+		require("lsp-progress").setup()
+	end,
+}
