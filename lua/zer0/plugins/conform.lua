60e7f05030c059a6f86ac001d0b36b67ce2ccc22
Marko Roman <marko1299@ukr.net>
2023-11-24 15:03:27 +0200

    Disable autoformat

--- lua/zer0/plugins/conform.lua
+++ lua/zer0/plugins/conform.lua
@@ -7,11 +7,6 @@ return {
 		"BufNewFile",
 	},
 	opts = {
-		format_on_save = {
-			-- These options will be passed to conform.format()
-			timeout_ms = 500,
-			lsp_fallback = true,
-		},
 		formatters_by_ft = vars.formatters,
 	},
 }
