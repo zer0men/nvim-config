d78e1f6ce2c80967ba8a5c3cd50f93f081e361a0
Marko Roman <marko1299@ukr.net>
2024-01-04 17:46:07 +0200

    Add zoxide

--- lua/zer0/plugins/telescope/extensions/repo.lua
+++ lua/zer0/plugins/telescope/extensions/repo.lua
@@ -1,26 +1,26 @@
 local telescope_tools = vim.g.telescope
 
 return {
-	"cljoly/telescope-repo.nvim",
-	module = true,
-	init = function()
-		telescope_tools.add_extension("repo", {
-			list = {
-				fd_opts = {},
-				settings = {
-					auto_lcd = true,
-				},
-			},
-		})
-		vim.g["rooter_cd_cmd"] = "lcd"
-	end,
-	keys = {
-		{
-			"<leader>fp",
-			function()
-				vim.cmd.Telescope("repo", "list")
-			end,
-			desc = "Find Projects",
-		},
-	},
+    "cljoly/telescope-repo.nvim",
+    module = true,
+    init = function()
+        telescope_tools.add_extension("repo", {
+            list = {
+                fd_opts = {},
+                settings = {
+                    auto_lcd = true,
+                },
+            },
+        })
+        vim.g["rooter_cd_cmd"] = "lcd"
+    end,
+    keys = {
+        {
+            "<leader>fp",
+            function()
+                vim.cmd.Telescope("repo", "list")
+            end,
+            desc = "Find Projects",
+        },
+    },
 }
