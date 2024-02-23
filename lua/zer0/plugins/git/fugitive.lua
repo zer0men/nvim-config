07b5b41c63d3534542b887a4e82765a2184861ec
Marko Roman <marko1299@ukr.net>
2024-02-23 11:30:20 +0200

    Add short mode to status

--- lua/zer0/plugins/git/fugitive.lua
+++ lua/zer0/plugins/git/fugitive.lua
@@ -4,7 +4,7 @@ return {
         "Git",
     },
     keys = {
-        { "<leader>gg", vim.cmd.Git,                          desc = "Git" },
-        { "<leader>gs", function() vim.cmd.Git("status") end, desc = "Git" },
+        { "<leader>gg", vim.cmd.Git,                                  desc = "Git" },
+        { "<leader>gs", function() vim.cmd.Git("status --short") end, desc = "Git" },
     },
 }
