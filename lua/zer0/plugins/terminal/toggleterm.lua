bb7d6e8230ab1325e575ff55727d7e9ce4fc8595
Marko Roman <marko1299@ukr.net>
2024-03-06 13:01:19 +0200

    Update toggle term

--- lua/zer0/plugins/terminal/toggleterm.lua
+++ lua/zer0/plugins/terminal/toggleterm.lua
@@ -1,7 +1,16 @@
 return {
     "akinsho/toggleterm.nvim",
     version = "*",
+
+    opts = {
+        open_mapping = "<leader>;",
+        insert_mappings = false,   -- whether or not the open mapping applies in insert mode
+        terminal_mappings = false, -- whether or not the open mapping applies in the opened terminals
+    },
     config = true,
+    keys = {
+        { "<leader>;", desc = "Toggle Term" },
+    },
     cmd = {
         "ToggleTerm"
     }
