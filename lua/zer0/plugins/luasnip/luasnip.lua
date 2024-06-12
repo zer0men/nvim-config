7d307b470e4e57eb7a9477c484aef80b02c4a101
Marko Roman <marko1299@ukr.net>
2024-06-12 23:40:15 +0300

    improve plugin load

--- lua/zer0/plugins/luasnip/luasnip.lua
+++ lua/zer0/plugins/luasnip/luasnip.lua
@@ -1,6 +1,5 @@
 return {
   {
-    lazy = true,
     "L3MON4D3/LuaSnip",
     dependencies = { "rafamadriz/friendly-snippets" },
     build = "make install_jsregexp",
--- lua/zer0/plugins/treesitter/init.lua
+++ lua/zer0/plugins/treesitter/init.lua
@@ -1,5 +1,6 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  event = "BufEnter",
   config = function()
     require("nvim-treesitter.configs").setup({
       ensure_installed = "all",
