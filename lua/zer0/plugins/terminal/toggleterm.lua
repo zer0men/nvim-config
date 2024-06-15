9ff19c682c331d9c0cd99a51e65e08c3bbd3f908
Marko Roman <marko1299@ukr.net>
2024-06-15 11:46:14 +0300

    Change shell of toggle term to zsh instead of tmux

--- lua/zer0/plugins/terminal/toggleterm.lua
+++ lua/zer0/plugins/terminal/toggleterm.lua
@@ -1,5 +1,6 @@
 math.randomseed(os.time())
-local shell = string.format('bash -c "tmux -L %f new"', math.random())
+-- local shell = string.format('bash -c "tmux -L %f new"', math.random())
+local shell = "zsh"
 
 return {
   "akinsho/toggleterm.nvim",
@@ -7,7 +8,14 @@ return {
 
   opts = {
     shell = shell,
-    direction = "float",
+    direction = "horizontal",
+    size = function(term)
+      if term.direction == "horizontal" then
+        return vim.o.lines * 0.38
+      elseif term.direction == "vertical" then
+        return vim.o.columns * 0.4
+      end
+    end,
     open_mapping = "<leader>;",
     insert_mappings = false, -- whether or not the open mapping applies in insert mode
     terminal_mappings = false, -- whether or not the open mapping applies in the opened terminals
