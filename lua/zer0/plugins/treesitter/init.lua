34b8ab2c9e32bfea0ecd20f5c6e1304040ea2af6
Marko Roman <marko1299@ukr.net>
2024-06-15 11:46:43 +0300

    Change treesitter load event

--- lua/zer0/plugins/treesitter/init.lua
+++ lua/zer0/plugins/treesitter/init.lua
@@ -1,6 +1,6 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  event = "BufEnter",
+  event = "VeryLazy",
   config = function()
     require("nvim-treesitter.configs").setup({
       ensure_installed = "all",
