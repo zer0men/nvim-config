af46b8af0b429451adfd2863e81a977c9c321d1d
Marko Roman <marko1299@ukr.net>
2024-07-03 16:11:41 +0300

    Add recomended config for noice

--- lua/zer0/plugins/noice.lua
+++ lua/zer0/plugins/noice.lua
@@ -7,9 +7,9 @@ return {
       lsp = {
         -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
         override = {
-          -- ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-          -- ["vim.lsp.util.stylize_markdown"] = true,
-          -- ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
         },
       },
       -- you can enable a preset for easier configuration
