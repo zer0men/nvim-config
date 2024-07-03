c0991cacfb121e317a9ad264463bf226f2f08536
Marko Roman <marko1299@ukr.net>
2024-07-03 16:38:47 +0300

    Add options to open folders related to opened file

--- lua/zer0/plugins/neo-tree.lua
+++ lua/zer0/plugins/neo-tree.lua
@@ -7,7 +7,10 @@ return {
     "MunifTanjim/nui.nvim",
     "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
   },
-  opts = {},
+  opts = {
+      reveal = true, -- path to file or folder to reveal
+      reveal_force_cwd = true,   -- change cwd without asking if needed
+  },
   cmd = {
     "Neotree",
   },
@@ -15,21 +18,21 @@ return {
     {
       "<leader>nn",
       function()
-        vim.cmd.Neotree("toggle", "float", "filesystem")
+        vim.cmd.Neotree("toggle", "float", "filesystem", "reveal", "reveal_force_cwd")
       end,
       desc = "NvimTree Toggle Buffers",
     },
     {
       "<leader>nb",
       function()
-        vim.cmd.Neotree("toggle", "float", "buffers")
+        vim.cmd.Neotree("toggle", "float", "buffers", "filesystem", "reveal", "reveal_force_cwd" )
       end,
       desc = "NvimTree Toggle Buffers",
     },
     {
       "<leader>ng",
       function()
-        vim.cmd.Neotree("toggle", "float", "git_status")
+        vim.cmd.Neotree("toggle", "float", "git_status", "reveal", "reveal_force_cwd")
       end,
       desc = "NvimTree Toggle Git",
     },
