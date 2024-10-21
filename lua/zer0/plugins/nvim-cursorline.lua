c0cbc5d749f33b77ffb2f7df1ebf0fe25780c148
Roman Marko <marko1299@ukr.net>
2024-10-21 23:45:55 +0300

    feat(nvim-cursorline): add cursorline plugin https://github.com/yamatsum/nvim-cursorline

---
+++ lua/zer0/plugins/nvim-cursorline.lua
@@ -0,0 +1,15 @@
+return {
+  "yamatsum/nvim-cursorline",
+  opts = {
+    cursorline = {
+      enable = false,
+      timeout = 1000,
+      number = false,
+    },
+    cursorword = {
+      enable = true,
+      min_length = 3,
+      hl = { underline = true },
+    },
+  },
+}
