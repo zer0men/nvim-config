891e6aac60f7c82b5ffefb0d45d9b8fb2d373bfa
Roman Marko <marko1299@ukr.net>
2024-10-21 10:32:32 +0300

    fix(git-worktree): update setup function because new version come

--- lua/zer0/plugins/git/git-worktree.lua
+++ lua/zer0/plugins/git/git-worktree.lua
@@ -4,21 +4,15 @@ return {
     "nvim-telescope/telescope.nvim",
   },
   config = function()
-    require("git-worktree").setup({
+    vim.g.git_worktree = {
       change_directory_command = "cd",
       update_on_change = true, -- default: true,
       update_on_change_command = "e .", -- default: "e .",
       clearjumps_on_change = true, -- default: true,
       autopush = false, -- default: false,
-    })
+    }
+    require("git-worktree")
 
-    local Worktree = require("git-worktree")
-
-    Worktree.on_tree_change(function(op, metadata)
-      if op == Worktree.Operations.Switch then
-        vim.notify("Switched from " .. metadata.prev_path .. " to " .. metadata.path)
-      end
-    end)
     require("telescope").load_extension("git_worktree")
   end,
   keys = {
