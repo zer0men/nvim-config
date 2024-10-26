607e826b71ea37b51aa719becf9be565f9ab2e20
Roman Marko <marko1299@ukr.net>
2024-10-26 15:41:49 +0300

    feat(luapad): add luapad settings and mapping

--- lua/zer0/plugins/luapad.lua
+++ lua/zer0/plugins/luapad.lua
@@ -1,3 +1,18 @@
 return {
   "rafcamlet/nvim-luapad",
+  opts = {},
+  config = function()
+    local luapad = require("luapad")
+    local luapad_run = require("luapad.run")
+    luapad.setup({
+      eval_on_move = false,
+      eval_on_change = false,
+    })
+    vim.keymap.set("n", "<leader>Ll", luapad.init)
+    vim.keymap.set("n", "<leader>Lr", luapad_run.run)
+  end,
+  keys = {
+    { "<leader>Ll", desc = "Luapad Open" },
+    { "<leader>Lr", desc = "Luapad Run" },
+  },
 }
