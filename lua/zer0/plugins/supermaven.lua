bd37c92e930817e5fdaa646697f787a37adae629
zer0men <masterzer0@ukr.net>
2024-11-11 20:12:24 +0200

    feat(supermaven): add supermaven plugin

---
+++ lua/zer0/plugins/supermaven.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "supermaven-inc/supermaven-nvim",
+    config = function()
+      local supermaven = require("supermaven-nvim")
+      local supermaven_api = require("supermaven-nvim.api")
+      supermaven.setup({})
+      vim.g.cmp.add_source("supermaven")
+      supermaven_api.stop()
+      vim.keymap.set("n", "<leader>ts", supermaven_api.toggle)
+    end,
+    keys = {
+      { "<leader>ts", desc = "Toggle Supermaven" },
+    },
+  },
+}
