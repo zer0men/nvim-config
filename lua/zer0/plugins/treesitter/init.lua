9ba4e96bf846e433ccb0d0c53383aca25d2f96d7
zer0men <masterzer0@ukr.net>
2024-11-18 15:02:36 +0200

    feat: install treesitter textobjects plugin

--- lua/zer0/plugins/treesitter/init.lua
+++ lua/zer0/plugins/treesitter/init.lua
@@ -1,17 +1,24 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  event = "VeryLazy",
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = "all",
-      sync_install = true,
-    })
-    vim.cmd.TSInstall("all")
-    vim.cmd.TSUpdate()
-    vim.cmd.TSEnable("highlight")
+  {
+    "nvim-treesitter/nvim-treesitter",
+    event = "VeryLazy",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = "all",
+        sync_install = true,
+      })
+      vim.cmd.TSInstall("all")
+      vim.cmd.TSUpdate()
+      vim.cmd.TSEnable("highlight")
 
-    vim.keymap.set("n", "<leader>tth", function()
-      vim.cmd.TSBufToggle("highlight")
-    end, { desc = "Toggle buffer Highlight" })
-  end,
+      vim.keymap.set("n", "<leader>tth", function()
+        vim.cmd.TSBufToggle("highlight")
+      end, { desc = "Toggle buffer Highlight" })
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    opts = {},
+  },
 }
