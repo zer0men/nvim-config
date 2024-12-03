79f6671eb2456da36e5f667b58696103eb6efbf0
zer0men <masterzer0@ukr.net>
2024-12-03 12:15:07 +0200

    chore: update php debug adapter config

--- lua/zer0/plugins/dap/configs/php.lua
+++ lua/zer0/plugins/dap/configs/php.lua
@@ -12,8 +12,5 @@ dap.configurations.php = {
     request = "launch",
     name = "Listen for Xdebug",
     port = 9003,
-    pathMappings = {
-      ["/var/www/ara.git/master"] = "${workspaceFolder}",
-    },
   },
 }
