0109cdcf5e9b81671bc827651db9b0d9a3fbef5c
zer0men <masterzer0@ukr.net>
2024-12-06 17:22:46 +0200

    feat: add colorschemes fzf

--- lua/zer0/plugins/fzf-lua.lua
+++ lua/zer0/plugins/fzf-lua.lua
@@ -5,5 +5,11 @@ return {
   config = function()
     -- calling `setup` is optional for customization
     require("fzf-lua").setup({})
+    vim.keymap.set("n", "<leader>fc", function()
+      vim.cmd.FzfLua("colorschemes")
+    end, { desc = "FZF colorsheme" })
   end,
+  keys = {
+    { "<leader>fc" },
+  },
 }
