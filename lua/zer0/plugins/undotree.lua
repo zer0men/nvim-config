6adb8a11794f49b00b6229618139d4b910604091
zer0 Men <masterzer0@ukr.net>
2024-12-09 18:15:15 +0200

    feat: add undotree focus after open undetree window

--- lua/zer0/plugins/undotree.lua
+++ lua/zer0/plugins/undotree.lua
@@ -1,6 +1,13 @@
 return {
   "mbbill/undotree",
   keys = {
-    { "<leader>u", vim.cmd.UndotreeToggle, desc = "UndoTree Toggle" },
+    {
+      "<leader>u",
+      function()
+        vim.cmd.UndotreeToggle()
+        vim.cmd.UndotreeFocus()
+      end,
+      desc = "UndoTree Toggle",
+    },
   },
 }
