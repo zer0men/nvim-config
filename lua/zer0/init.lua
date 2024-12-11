6441a8cd4302ac59e792213d772d4cc2e8b07534
zer0 Men <masterzer0@ukr.net>
2024-12-11 14:44:04 +0200

    refactor: rewrite imports

--- lua/zer0/init.lua
+++ lua/zer0/init.lua
@@ -1,14 +1,8 @@
-local main_module = "zer0"
-
-local function zer0_require(name)
-  require(main_module .. "." .. name)
-end
-
-zer0_require("start_setup")
-zer0_require("globals")
-zer0_require("options")
-zer0_require("lazy")
-zer0_require("autocommands")
-zer0_require("mapping")
-zer0_require("diagnostics")
-zer0_require("finish_setup")
+require("zer0.start_setup")
+require("zer0.globals")
+require("zer0.options")
+require("zer0.lazy")
+require("zer0.autocommands")
+require("zer0.mapping")
+require("zer0.diagnostics")
+require("zer0.finish_setup")
