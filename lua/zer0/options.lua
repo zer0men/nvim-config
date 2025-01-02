536d19a8e8fef064685fbe0673121db99ee7d82f
zer0 Men <masterzer0@ukr.net>
2025-01-02 09:11:16 +0200

    feat: add options from primagen config

--- lua/zer0/options.lua
+++ lua/zer0/options.lua
@@ -27,6 +27,7 @@ local options = {
   updatetime = 50,
   colorcolumn = "80",
   shortmess = "ltToOCFI",
+  wrap = false,
 }
 
 for key, value in pairs(options) do
@@ -40,3 +41,36 @@ end
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = ","
+vim.g.loaded_matchparen = 1 -- disable built-in MatchParen.
+
+vim.opt.guicursor = ""
+
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "80"
+
