d76fb45f1e11e0fcf4a1f63ecff4bd7f885bea2f
zer0 Men <masterzer0@ukr.net>
2025-01-05 19:26:59 +0200

    fix: fix profiling functions

--- lua/zer0/scripts/profiler.lua
+++ lua/zer0/scripts/profiler.lua
@@ -1,15 +1,27 @@
 local pid = vim.fn.getpid()
 
+local is_profiling = false
+
 local function profiler_start()
+  if is_profiling then
+    return
+  end
   local profiler_file = "/tmp/neovim-" .. pid .. ".profile"
-  vim.cmd.profiler("start", profiler_file)
-  vim.cmd.profiler("func", "*")
-  vim.cmd.profiler("file", "*")
+  vim.cmd.profile("start", profiler_file)
+  vim.cmd.profile("func", "*")
+  vim.cmd.profile("file", "*")
+  vim.notify("Profiler started")
+  is_profiling = true
 end
 
 local function profiler_stop()
-  vim.cmd.profiler("stop")
+  if not is_profiling then
+    return
+  end
+  vim.cmd.profile("stop")
+  vim.notify("Profiler stoped")
+  is_profiling = false
 end
 
 vim.keymap.set("n", "<leader>ps", profiler_start, { desc = "Profiler start" })
-vim.keymap.set("n", "<leader>pc", profiler_stop, { desc = "Profiler start" })
+vim.keymap.set("n", "<leader>pc", profiler_stop, { desc = "Profiler stop" })
